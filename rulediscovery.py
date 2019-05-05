25da1fac65654be42d08526ae8ae7b470f178aaa
Alex Nuetzel <karlanuetzel@gmail.com>
2019-05-04 19:48:29 -0500

    much needed changes, still have more to do, 85% accurate

--- rulediscovery.py
+++ rulediscovery.py
@@ -78,101 +78,70 @@ def ruleDiscovery(validationSet, validationRep):
     associationList = []
     associationAttributeIndex = 0
     associationAttributeValueIndex = 1
-    associationClassIndex = 2
-    associationTidsIndex = 3
+    associationTidsIndex = 2
     
     #attrIdx is the index of the corresponding attribute (A, B, C, D)
     #idx is which index of the value (A1, B2, B3)
     for attrIdx, attr in enumerate(validationRep):
         for idx in range (attributePossibleValues):
             specificAttributeTIDs = attr[idx]
-            totalTimesAttributeSeen = len(specificAttributeTIDs)    
-                                                
+            totalTimesAttributeSeen = len(specificAttributeTIDs)                                        
             if totalTimesAttributeSeen > 0:
-                tidsC1 = list(set(specificAttributeTIDs).intersection(legitimateIndexes))
-                frequencyC1 = len(tidsC1)
-                tidsC2 = list(set(specificAttributeTIDs).intersection(suspiciousIndexes))
-                frequencyC2 = len(tidsC2)
-                tidsC3 = list(set(specificAttributeTIDs).intersection(phishyIndexes))
-                frequencyC3 = len(tidsC3)
-                
-                confidenceC1 = float(frequencyC1 / totalTimesAttributeSeen)
-                confidenceC2 = float(frequencyC2 / totalTimesAttributeSeen)
-                confidenceC3 = float(frequencyC3 / totalTimesAttributeSeen)
-                
-                supportC1 = float(frequencyC1 / validationSetSize)
-                supportC2 = float(frequencyC2 / validationSetSize)
-                supportC3 = float(frequencyC3 / validationSetSize)
-                
                 #here is where we start looking for multi-attribute associations
                 tempAssociationList = []
                 for association in associationList:
                     #this could get a little hairy....
-                    classID = association[associationClassIndex]
                     attributeIdxList = association[associationAttributeIndex]
                     attributeValIdxList = association[associationAttributeValueIndex]
                     tids = association[associationTidsIndex]
-                    
-                    frequency = 0
-                    if classID == legitimate:
-                        frequency = frequencyC1
-                        newTids = tidsC1
-                    if classID == suspicious:
-                        frequency = frequencyC2
-                        newTids = tidsC2
-                    if classID == phishy:
-                        frequency = frequencyC3
-                        newTids = tidsC3
-                    if frequency > 0:
-                        sharedTids = list(set(tids).intersection(newTids))
-                        support = len(tidsC1) + len(tidsC2) + len(tidsC3)
-                        frequencyOfNewAssociation = 0
-                        bigBoyTids = []
-                        for tid in sharedTids:
-                            data = validationSet[tid]
-                            if data[labelIndex] == classID:
-                                bigBoyTids.append(tid)
-                                frequencyOfNewAssociation += 1
-                        if frequencyOfNewAssociation > 0:
-                            newAssociationIdxList = attributeIdxList + [attrIdx]
-                            newAssociationIdxValList = attributeValIdxList + [idx]
-                            newAssociationTidsList = bigBoyTids
-                            assoc = [newAssociationIdxList, newAssociationIdxValList, classID, bigBoyTids]
-                            tempAssociationList.append(assoc)                                
-                            
-                            conf = float(frequencyOfNewAssociation / support)
-                            supp = float(frequencyOfNewAssociation / validationSetSize)
-                            decision = pruner(supp, conf)
-                            if decision:
-                                length = len(bigBoyTids)
-                                genRule = createRule(newAssociationIdxList, newAssociationIdxValList, classID, conf, supp, frequencyOfNewAssociation, length)
-                                ruleList.append(genRule)
-                                
+                    sharedTids = list(set(tids).intersection(specificAttributeTIDs))
+                    sharedTidsSize = len(sharedTids)
+                    if sharedTidsSize > 0:
+                        newAssociationIdxList = attributeIdxList + [attrIdx]
+                        newAssociationIdxValList = attributeValIdxList + [idx]
+                        assoc = [newAssociationIdxList, newAssociationIdxValList, sharedTids]
+                        tempAssociationList.append(assoc)    
+                        
                 if len(tempAssociationList) > 0:
                     associationList = associationList + tempAssociationList
-                if frequencyC1 > 0:
-                    association = [[attrIdx], [idx], legitimate, tidsC1]
-                    associationList.append(association)
-                if frequencyC2:
-                    association = [[attrIdx], [idx], suspicious, tidsC2]
-                    associationList.append(association)
-                if frequencyC3:
-                    association = [[attrIdx], [idx], phishy, tidsC3]
-                    associationList.append(association)
-                decisionC1 = pruner(supportC1, confidenceC1)
-                decisionC2 = pruner(supportC2, confidenceC2)
-                decisionC3 = pruner(supportC3, confidenceC3)
+                association = [[attrIdx], [idx], specificAttributeTIDs]
+                associationList.append(association)
                 
-                if decisionC1:
-                    length = len(tidsC1)
-                    genRule = createRule([attrIdx], [idx], legitimate, confidenceC1, supportC1, frequencyC1, length)
-                    ruleList.append(genRule)
-                if decisionC2:
-                    length = len(tidsC2)
-                    genRule = createRule([attrIdx], [idx], suspicious, confidenceC2, supportC2, frequencyC2, length)
-                    ruleList.append(genRule)
-                if decisionC3:
-                    length = len(tidsC3)
-                    genRule = createRule([attrIdx], [idx], phishy, confidenceC3, supportC3, frequencyC3, length)
-                    ruleList.append(genRule)
+    for association in associationList:
+        associationAttrIdx = association[associationAttributeIndex]
+        associationIdx = association[associationAttributeValueIndex]
+        associationTIDs = association[associationTidsIndex]
+        totalTIDs = len(associationTIDs)
+        tidsC1 = list(set(associationTIDs).intersection(legitimateIndexes))
+        frequencyC1 = len(tidsC1)
+        tidsC2 = list(set(associationTIDs).intersection(suspiciousIndexes))
+        frequencyC2 = len(tidsC2)
+        tidsC3 = list(set(associationTIDs).intersection(phishyIndexes))
+        frequencyC3 = len(tidsC3)
+    
+        confidenceC1 = float(frequencyC1 / totalTIDs)
+        confidenceC2 = float(frequencyC2 / totalTIDs)
+        confidenceC3 = float(frequencyC3 / totalTIDs)
+    
+        supportC1 = float(frequencyC1 / validationSetSize)
+        supportC2 = float(frequencyC2 / validationSetSize)
+        supportC3 = float(frequencyC3 / validationSetSize)
+        
+        decisionC1 = pruner(supportC1, confidenceC1)
+        decisionC2 = pruner(supportC2, confidenceC2)
+        decisionC3 = pruner(supportC3, confidenceC3)        
+        
+        if decisionC1:
+            length = len(tidsC1)
+            genRule = createRule(associationAttrIdx, associationIdx, legitimate, confidenceC1, supportC1, frequencyC1, length)
+            ruleList.append(genRule)
+        if decisionC2:
+            length = len(tidsC2)
+            genRule = createRule(associationAttrIdx, associationIdx, suspicious, confidenceC2, supportC2, frequencyC2, length)
+            ruleList.append(genRule)
+        if decisionC3:
+            length = len(tidsC3)
+            genRule = createRule(associationAttrIdx, associationIdx, phishy, confidenceC3, supportC3, frequencyC3, length)
+            ruleList.append(genRule)        
+            
     return ruleList
